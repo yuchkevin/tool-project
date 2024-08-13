e45459aee1d20c7cf36c6d849ee01ebad6013d32
yuch <ych4912@163.com>
2024-08-13 17:11:51 +0800

    初始化

---
+++ src/main/java/com/yuch/ToolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuch/ToolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
